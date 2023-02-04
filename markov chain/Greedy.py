4a3069d0b4dab386d567d888ba3d9e4aa598feb1
Giulio Pirotta <124442367+ggiuliopirotta@users.noreply.github.com>
2023-02-04 14:04:24 +0100

    Create Greedy.py

---
+++ markov chain/Greedy.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+def greedy(prob, iters, restarts, seed=None):
+    if seed is not None:
+        np.random.seed(seed)
+
+    best = None
+    c_min = np.inf
+
+    for _ in range(restarts):
+        prob.init_config()
+        c = prob.compute_cost()
+        print(f'initial cost: {c}')
+
+        for _ in range(iters):
+            move = prob.propose_move()
+            c_delta = prob.c_delta(move)
+
+            if c_delta <= 0:
+                prob.accept_move(move)
+                c += c_delta
+
+            if c == 0:
+                break
+
+        if c < c_min:
+            c_min = c
+            best = prob.copy()
+
+            if c == 0:
+                break
+    
+    print (f'min cost: {c_min}')
+    best.display()
+    return best
