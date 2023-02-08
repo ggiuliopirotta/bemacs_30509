c68a11bc0ebfed0e326f4ca01f603d046c454b3f
Giulio Pirotta <124442367+ggiuliopirotta@users.noreply.github.com>
2023-02-08 13:50:29 +0100

    Update WMC.py

--- markov chain/weighted max cut/WMC.py
+++ markov chain/weighted max cut/WMC.py
@@ -8,20 +8,20 @@ class Wmc:
         self.n = n
         self.x, self.y = rnd.random(n), rnd.random(n)
 
-        self.m = self.config_matrix(prob)
-        self.p = [[], []]
+        self.partition = [[], []]
+        self.dist = self.config_dist(prob)
 
-    def config_matrix(self, prob):
+    def config_dist(self, prob):
         n, x, y = self.n, self.x, self.y
 
-        mat = np.zeros((n, n))
+        dist = np.zeros((n, n))
         for i in range(n):
             for j in range(i+1, n):
                 if rnd.random() < prob:
-                    mat[i, j] = np.sqrt((x[i]-x[j])**2 + (y[i]-y[j])**2)
-                    mat[j, i] = mat[i, j]
+                    dist[i, j] = np.sqrt((x[i]-x[j])**2 + (y[i]-y[j])**2)
+                    dist[j, i] = dist[i, j]
 
-        return mat
+        return dist
     
     def init_config(self):
         n = self.n
@@ -29,54 +29,52 @@ class Wmc:
         part1 = np.sort(rnd.choice(np.arange(n), rnd.randint(1, n-1), replace=False))
         part2 = np.delete(np.arange(n), part1)
 
-        self.p[0], self.p[1] = part1, part2
+        self.partition[0], self.partition[1] = part1, part2
     
     def propose_move(self):
-        p = self.p
+        partition = self.partition
 
         p_choice = rnd.randint(2)
-        if len(p[p_choice]) <= 1:
+        if len(partition[p_choice]) <= 1:
             p_choice = 1-p_choice
 
-        p_from = p[p_choice]
+        p_from = partition[p_choice]
         v_ind = rnd.choice(len(p_from))
         v = p_from[v_ind]
 
         return v_ind, v, p_choice
 
     def accept_move(self, move):
-        p = self.p
+        partition = self.partition
         v_ind, v, p_choice = move
 
-        p_from, p_to = p[p_choice], p[1-p_choice]
+        p_from, p_to = partition[p_choice], partition[1-p_choice]
         p_from, p_to = np.delete(p_from, v_ind), np.sort(np.append(p_to, v))
-        p[p_choice], p[1-p_choice] = p_from, p_to
-
-        return self
+        self.partition[p_choice], self.partition[1-p_choice] = p_from, p_to
     
     def compute_delta_cost(self, move):
-        m, p = self.m, self.p
+        partition, dist = self.partition, self.dist
         v, p_choice = move[1], move[2]
 
-        w_old = np.sum(m[v, p[1-p_choice]])
-        w_new = np.sum(m[v, p[p_choice]])
+        w_old = np.sum(dist[v, partition[1-p_choice]])
+        w_new = np.sum(dist[v, partition[p_choice]])
 
         w_delta = w_new-w_old
         return w_delta
 
     def compute_cost(self):
-        n, m, p = self.n, self.m, self.p
+        n, partition, dist = self.n, self.partition, self.dist
 
         w = 0.0
         for i in range(n):
             for j in range(i+1, n):
-                if (i in p[0] and j in p[1]) or (i in p[1] and j in p[0]):
-                    w += m[i, j]
+                if (i in partition[0] and j in partition[1]) or (i in partition[1] and j in partition[0]):
+                    w += dist[i, j]
 
         return w
 
     def display(self):
-        n, x, y, m, p = self.n, self.x, self.y, self.m, self.p
+        n, x, y, partition, dist = self.n, self.x, self.y, self.partition, self.dist
 
         plt.pause(0.01)
         plt.figure(1)
@@ -84,11 +82,11 @@ class Wmc:
 
         for i in range(n):
             for j in range(i+1, n):
-                if m[i, j] != 0:
+                if dist[i, j] != 0:
                     xx = [x[i], x[j]]
                     yy = [y[i], y[j]]
 
-                    if (i in p[0] and j in p[1]) or (i in p[1] and j in p[0]):
+                    if (i in partition[0] and j in partition[1]) or (i in partition[1] and j in partition[0]):
                         plt.plot(xx, yy, color='green', alpha=0.5)
 
                     else:
